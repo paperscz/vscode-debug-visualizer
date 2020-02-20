b6b4f365c84dbabb4111c0fc4d77190fe194494b
Henning Dieterichs <henning.dieterichs@live.de>
2020-02-20 12:50:13 +0100

    WIP

---
+++ demo/java/DemoLinkedLists.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+import org.json.simple.JSONObject;
+
+public class DemoLinkedLists {
+    public static void main(String[] args) throws Exception {
+        JSONObject obj = new JSONObject();
+        System.out.print(obj);
+    }
+}
+
+public class Graph {
+    public Graph() {
+        nodes = new ArrayList();
+    }
+
+    public ArrayList<Node> nodes;
+}
+
+class Node {
+
+}
